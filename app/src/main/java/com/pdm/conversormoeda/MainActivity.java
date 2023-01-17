09939521758e13f0f2697d6d047e067df4a2d9df
ataidechilaule <ataide1chilaule@gmail.com>
2023-01-17 11:13:54 +0200

    Initial commit

---
+++ app/src/main/java/com/pdm/conversormoeda/MainActivity.java
@@ -0,0 +1,32 @@
+package com.pdm.conversormoeda;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void converter(View view){
+
+        EditText meticalEditText = findViewById(R.id.metical_txt);
+
+        TextView resultadoTexT = findViewById(R.id.result_txt);
+
+        if(!meticalEditText.getText().toString().equals("")){
+            float meticalValor = Float.parseFloat(meticalEditText.getText().toString());
+            float iene = meticalValor * 2F;
+
+            resultadoTexT.setText(String.valueOf(iene));
+        }else {
+            resultadoTexT.setText(R.string.emptyvalue);
+        }
+    }
+}
